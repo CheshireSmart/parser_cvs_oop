5d0c094d1777930a2a7148b186a51a5a4a7e71e1
Ilya <videospy-23@yandex.ru>
2022-04-18 16:37:33 +0500

    added CommitTest.txt to the repo

---
+++ src/main/java/parser_cvs/App.java
@@ -0,0 +1,13 @@
+package parser_cvs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
